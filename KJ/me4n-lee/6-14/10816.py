7f243ed8498941eb2904a77a485f53d2fcc4469f
keyleeu <minhk.lee21@gmail.com>
2023-06-14 23:52:54 +0900

    #6/14 11:52pm

---
+++ KJ/me4n-lee/6-14/10816.py
@@ -0,0 +1,60 @@
+# https://www.acmicpc.net/problem/10816
+# 숫자 카드
+# 10816
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+n_list = list(map(int, input().split()))
+
+m = int(input())
+
+# n_list = []
+# for _ in range(len(n_list)):
+
+m_list = list(map(int, input().split()))
+
+# print(n_list)
+# print(m_list)
+
+n_list.sort()
+
+print(n_list)
+
+dp = [0] * m
+
+def fun():
+
+    for j in range(m):
+        
+        num = m_list[j]
+        start = 0
+        end = n
+
+        if n_list[start] == num:
+            dp[j] += 1
+
+        if n_list[end-1] == num:
+            dp[j] += 1
+
+        while start >= end:
+            mid = (start + end) // 2
+            now = n_list[mid]
+                
+            if now >= num:    
+                start = mid - 1
+                if now == num:
+                    dp[j] += 1
+
+            else:
+                end = mid + 1
+
+    return dp
+            
+result = fun()
+print(result)
+
+for i in range(len(result)):
+    print(result[i], end= " ")
