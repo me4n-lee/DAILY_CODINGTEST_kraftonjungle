3ad0251ceb3e1b3c4e77ac3acc8b081d038dacec
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-07-08 00:14:47 +0900

    #7/8 00:14 문제 풀이 O

--- KJ/creamcheezecat/July/7-7/6603.py
+++ KJ/creamcheezecat/July/7-7/6603.py
@@ -1,6 +1,32 @@
 """ 
-
+https://www.acmicpc.net/problem/6603<br/>
+로또<br/>
+6603
 """
 
+from itertools import combinations
 import sys
 input = sys.stdin.readline
+
+def fun(arr):
+    ncount = len(arr)
+    result = [0] * 6
+    for combination in combinations(arr, 6):
+        result[:6] = combination
+
+        # 조합 출력
+        print(*result)
+
+
+while(1):
+    tc = list(map(int,input().split()))
+    if tc[0]== 0:
+        break
+    del tc[0]
+    
+    fun(tc)
+    print()
+    
+    
+#print(tc)
+
