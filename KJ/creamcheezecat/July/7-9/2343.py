4cd820fe33b6a3b37f4602595021f5a73471307d
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-07-10 01:06:08 +0900

    #7/10 01:05 문제 풀이 X

--- KJ/creamcheezecat/July/7-9/2343.py
+++ KJ/creamcheezecat/July/7-9/2343.py
@@ -6,3 +6,16 @@ https://www.acmicpc.net/problem/2343<br/>
 
 import sys
 input = sys.stdin.readline
+
+
+n,m = map(int,input().split())
+bluelay = list(map(int,input().split()))
+
+l = 0
+r = bluelay[-1]
+total = 1
+sol = 0
+while l < r :
+    m = (l + r)//2
+    
+    
