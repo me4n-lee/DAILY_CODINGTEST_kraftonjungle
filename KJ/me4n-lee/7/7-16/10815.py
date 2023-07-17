3347c75dc369607684592690e69824672ea48803
keyleeu <minhk.lee21@gmail.com>
2023-07-18 02:27:39 +0900

    7/18 O

--- KJ/me4n-lee/7/7-16/10815.py
+++ KJ/me4n-lee/7/7-16/10815.py
@@ -4,4 +4,48 @@
 
 import sys
 input = sys.stdin.readline
-from itertools import permutations
+
+n = int(input())
+n_list = list(map(int, input().split()))
+m = int(input())
+m_list = list(map(int, input().split()))
+
+n_list.sort()
+
+# print(n_list)
+# print(m_list)
+
+def fun():
+
+    answer = []
+
+    for i in range(m):
+
+        result = 0
+
+        now = m_list[i]
+
+        start = 0
+        end = n - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if n_list[mid] < now:
+                start = mid + 1
+
+            elif n_list[mid] > now:
+                end = mid - 1
+
+            else:
+                result = 1
+                break
+        
+        answer.append(result)
+
+    return answer
+
+final = fun()
+for i in range(m):
+    print(final[i], end = " ")
+        
