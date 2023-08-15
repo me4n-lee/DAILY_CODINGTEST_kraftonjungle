a998ab50160c2074646fae1ce08268d539d41475
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-08-15 14:26:06 +0900

    못 푼 문제들 되짚어 보기 추가

--- KJ/creamcheezecat/no_solve/7-22/16930.py
+++ KJ/creamcheezecat/no_solve/7-22/16930.py
@@ -12,7 +12,47 @@ from collections import deque
 # 매초 마다 한 방향을 1 ~ K 칸 이동
 # 최소 시간
 
-N,M,K = map(int , input().split())
+# BFS
+def bfs(x1, y1, x2, y2):
+  queue = deque()
+  # 시작점 삽입, 방문 처리
+  queue.append((x1, y1))
+  graph[x1][y1] = 0
+
+  while queue:
+    x, y = queue.popleft()
+    # 끝점에 도착했으면 종료
+    if(x == x2 and y == y2):
+      return graph[x2][y2]
+    # 상하좌우 탐색
+    for i in range(4):
+      for j in range(1, k + 1):
+        nx = x + dx[i] * j
+        ny = y + dy[i] * j
+        # 범위 내이고
+        if(0 <= nx < n and 0 <= ny < m):
+          # 방문하지 않은 빈칸이면
+          if(graph[nx][ny] == '.'):
+            # 삽입, 방문 처리
+            queue.append((nx, ny))
+            graph[nx][ny] = graph[x][y] + 1
+          elif(graph[nx][ny] == '#'):
+            break
+        else: break
+  return -1
+
+n, m, k = map(int, input().split())  # 세로, 가로, 1초에 이동가능한 최대 칸
+# n * m (.: 빈칸 #: 벽)
+graph = [list(input()) for _ in range(n)]
+x1, y1, x2, y2 = map(int, input().split())  # 시작점, 끝점
+visited = [[0] * m for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs(x1-1, y1-1, x2-1, y2-1))
+
+
+""" N,M,K = map(int , input().split())
 
 board = []
 #visited = [[False for _ in range(N)] for _ in range(M)]
@@ -38,8 +78,8 @@ def bfs():
         
         for i in range(4):
             for k in range(1, K + 1):
-                nx = x + (dx[i] * k)
-                ny = y + (dy[i] * k)
+                nx = x + dx[i] * k
+                ny = y + dy[i] * k
 
                 if 0 <= nx < M and 0 <= ny < N and not visited[ny][nx]:
                     if board[ny][nx] == '#':
@@ -54,4 +94,4 @@ def bfs():
     
     return -1
 
-print(bfs())
+print(bfs()) """
