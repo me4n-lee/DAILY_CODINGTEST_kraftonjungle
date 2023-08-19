5efd64becd2f8179d750d2dc234c98a134ad7e26
creamcheezecat <92340465+creamcheezecat@users.noreply.github.com>
2023-08-19 20:58:55 +0900

    test

--- KJ/creamcheezecat/no_solve/data/number.py
+++ KJ/creamcheezecat/no_solve/data/number.py
@@ -1,13 +1,25 @@
 """ 
-https://www.acmicpc.net/problem/2643
-색종이 올려 놓기
-2643
+
 """
 
 import sys
 input = sys.stdin.readline
 
-# 크기가 다른 직사각형 색종이가 여러 장 있다.
-
 N = int(input())
+K = int(input())
 
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+for i in range(N + 1):
+    dp[i][0] = 1
+    dp[i][1] = i
+    
+for i in range(2, N + 1):
+    for j in range(2, K + 1):
+        if i == N:
+            dp[i][j] = dp[i - 3][j - 1] + dp[i - 1][j]
+        else:
+            dp[i][j] = dp[i - 1][j] + dp[i - 2][j - 1]
+        dp[i][j] %= 1000000003
+        
+print(dp[N][K])
